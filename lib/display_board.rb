af750c73c8f6ad9452506c1ba90a78a3e8a6d61c
Andrew Caniglia <andrewcaniglia@gmail.com>
2018-04-24 10:41:54 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-display_board =
-print 
+def display_board
+print    |   |
+print -----------
+print    |   |
+print -----------
+print    |   |
+end
